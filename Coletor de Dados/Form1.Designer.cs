4938c5bccbeee9b255a3ba15b58fec2674a2a1d7
Vitor Hugo <vitorhugopilger@gmail.com>
2023-04-20 10:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ Coletor de Dados/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Coletor_de_Dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Coletor de Dados/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coletor_de_Dados
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        private PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        private PerformanceCounter diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+
+        private string csvPath = "./" + SystemInformation.ComputerName + "_Output.csv";
+
+        private bool isRunning = true;
+        private int timeOut = 1000;
+
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+        public Form1()
+        {
+            InitializeComponent();
+            SystemTrayIcon();
+            BackgroundCollector();
+
+            button1.Text = isRunning.ToString();
+        }
+
+        private void SystemTrayIcon()
+        {
+            notifyIcon.Visible = true;
+            notifyIcon.Text = "Coletor de Dados";
+            notifyIcon.BalloonTipText = "O Coletor de Dados está executando em segundo plano";
+        }
+
+        private void BackgroundCollector()
+        {
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (backgroundWorker.CancellationPending)
+            {
+                // Define a propriedade Cancel como true para indicar que a tarefa foi cancelada
+                e.Cancel = true;
+                Console.WriteLine(backgroundWorker.CancellationPending);
+                return;
+            }
+            else
+            {
+                Console.WriteLine(backgroundWorker.IsBusy);
+
+                while (isRunning)
+                {
+                    // Coleta os valores dos PerformanceCounters
+                    var cpu = cpuCounter.NextValue();
+                    var memory = ramCounter.NextValue();
+                    var disk = diskCounter.NextValue();
+
+                    // Grava os valores no arquivo CSV
+                    using (var writer = new StreamWriter(csvPath, true))
+                    {
+                        writer.WriteLine($"{DateTime.Now},{Math.Round(cpu)},{Math.Round(memory)},{Math.Round(disk)}");
+                    }
+                    Console.WriteLine($"{DateTime.Now},{Math.Round(cpu)},{Math.Round(memory)},{Math.Round(disk)}");
+                    Thread.Sleep(timeOut);
+                }
+            }
+        }
+
+        private void notifyIcon_Click_1(object sender, EventArgs e)
+        {
+            Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                backgroundWorker.CancelAsync();
+            }
+            else
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+            isRunning = !isRunning;
+            button1.Text = isRunning.ToString();
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+                notifyIcon.ShowBalloonTip(1500);
+            }
+        }
+    }
+}
